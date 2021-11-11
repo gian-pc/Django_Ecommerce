85e13dccd24f67d697b7a6be10d88c4c05697ef0
Gian Carlos <gpaucarcortez@gmail.com>
2021-11-11 00:33:52 -0500

    Product model

--- myecommerce/ecommerce_api/models.py
+++ myecommerce/ecommerce_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import activate
 
 # Create your models here.
 
@@ -21,5 +22,19 @@ class Brand(models.Model):
     image = models.ImageField(upload_to = "brands", null = False)
     brand = models.CharField(max_length = 200)
     activated = models.BooleanField()
+
     def __str__(self):
         return self.brand
+
+class Product(models.Model):
+    name = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 200)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to = "products", null = False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    activated = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
